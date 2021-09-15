fc83e10bb9a7f0c2143a957f70b41c276d7c63c3
ponta027 <pont027@gmail.com>
2021-09-16 00:19:05 +0900

    refactoring plot code.

--- util/plotters_sample/src/main.rs
+++ util/plotters_sample/src/main.rs
@@ -1,23 +1,41 @@
+use plotters::drawing::IntoDrawingArea;
 use plotters::prelude::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("0.png", (640, 480)).into_drawing_area();
+/** */
+fn sample_plot2(name: String) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new(&name, (640, 480)).into_drawing_area();
     root.fill(&WHITE)?;
+
     let mut chart = ChartBuilder::on(&root)
         .caption("y=x^2", ("sans-serif", 50).into_font())
-        .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
+        .build_cartesian_2d(-0f32..2f32, -0.0f32..2f32)?;
 
     chart.configure_mesh().draw()?;
 
+    let points: Vec<(f32, f32)> = (-50..=50)
+        .map(|x| x as f32 / 50.0)
+        .map(|x| (x, x * x))
+        .collect();
+    let points_sine: Vec<(f32, f32)> = (-100..=100)
+        .map(|x| x as f32)
+        .map(|x| (x / 100., x.sin()))
+        .collect();
+
+    let data_points: Vec<(f32, f32)> = (0..=10).map(|x| x as f32 / 10.0).map(|x| (x, x)).collect();
+
     chart
-        .draw_series(LineSeries::new(
-            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
-            &RED,
-        ))?
-        .label("y = x^2")
+        .draw_series(LineSeries::new(data_points.into_iter(), &BLUE))?
+        .label("y = x")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    chart
+        .draw_series(LineSeries::new(points.into_iter(), &BLUE))?
+        .label("y = x^2 ")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    chart
+        .draw_series(LineSeries::new(points_sine.into_iter(), &GREEN))?
+        .label("y = sin(x) ")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
 
     chart
@@ -26,5 +44,58 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .border_style(&BLACK)
         .draw()?;
 
+    Ok(())
+}
+
+/** */
+struct GraphInfo {
+    width: u32,
+    height: u32,
+    name: String,
+    caption: String,
+}
+
+/** */
+fn plot_data(graph: &GraphInfo, data: Vec<(f32, f32)>) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new(&graph.name, (graph.width, graph.height)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption(&graph.caption, ("sans-serif", 50).into_font())
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(-0f32..2f32, -0.0f32..2f32)?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(data.into_iter(), &BLUE))?
+        .label("y = x")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+    Ok(())
+}
+
+/** */
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let points_sine: Vec<(f32, f32)> = (-10..=10)
+        .map(|x| x as f32)
+        .map(|x| (x / 10., x.sin()))
+        .collect();
+
+    let graph1 = GraphInfo {
+        width: 640,
+        height: 480,
+        name: "sine_curve.png".to_string(),
+        caption: "sine curve".to_string(),
+    };
+    plot_data(&graph1, points_sine).unwrap();
+
+    sample_plot2("sample_plot2.png".to_string()).unwrap();
     Ok(())
 }
