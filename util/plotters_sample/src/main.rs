2e7b4116e4215efc333fb7a23dbd9742abe37912
ponta027 <pont027@gmail.com>
2021-11-22 23:46:25 +0900

    refactoring.

--- util/plotters_sample/src/main.rs
+++ util/plotters_sample/src/main.rs
@@ -1,17 +1,16 @@
-
 mod plot;
 
 /** */
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let name = "png/sine_curve.png";
     let points_sine: Vec<(f32, f32)> = (-10..=10)
         .map(|x| x as f32)
         .map(|x| (x / 10., x.sin()))
         .collect();
     let points2: Vec<(f32, f32)> = (-10..=10)
         .map(|x| x as f32)
-        .map(|x| (x / 10., x/10.))
+        .map(|x| (x / 10., x / 10.))
         .collect();
-
 
     let data = plot::GraphData {
         name: "sample".to_string(),
@@ -22,17 +21,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         points: &points2,
     };
 
-
     let graph1 = plot::GraphInfo {
         width: 640,
         height: 480,
-        name: "sine_curve.png".to_string(),
+        name: name.to_string(),
         caption: "sine curve".to_string(),
-        points: &vec![data,data2],
+        points: &vec![data, data2],
     };
 
-    plot::sample_plot2("sample_plot2.png".to_string()).unwrap();
+    // plot::sample_plot2(name.to_string()).unwrap();
 
     plot::plot_data(&graph1).unwrap();
+
+    {
+        let name2 = "png/sine_curve2.png";
+        let points_sine2: Vec<(f32, f32)> = (-20..=20)
+            .map(|x| x as f32)
+            .map(|x| (x / 10., x.sin()))
+            .collect();
+        let data2 = plot::GraphData {
+            name: "sample".to_string(),
+            points: &points_sine2,
+        };
+        let graph2 = plot::GraphInfo {
+            width: 640,
+            height: 480,
+            name: name2.to_string(),
+            caption: "sine curve2".to_string(),
+            points: &vec![data2],
+        };
+
+        plot::plot_data(&graph2).unwrap();
+    }
     Ok(())
 }
