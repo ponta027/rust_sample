fbb71499013ebd622166ee1f7a24a58e373cd002
ponta027 <ponta027@gmail.com>
2022-05-03 12:05:57 +0900

    modify index

--- web/rocket_sample/src/main.rs
+++ web/rocket_sample/src/main.rs
@@ -1,12 +1,10 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+mod routes;
+mod models;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![routes::index])
 }
